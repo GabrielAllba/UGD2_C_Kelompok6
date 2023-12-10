68d37292a916cfd127591134b07d1eadfb07b125
vipermint04 <pjenniakjennia@gmail.com>
2023-12-11 00:56:46 +0700

    new_item

--- lib/components/item_prof.dart
+++ lib/components/item_prof.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:ugd2_c_kelompok6/client/AuthClient.dart';
+import 'package:ugd2_c_kelompok6/database/pemesanan/sql_helper.dart';
 import 'package:ugd2_c_kelompok6/entity/User.dart';
 import 'package:ugd2_c_kelompok6/screens/editProfile.dart';
 
+import 'package:image_picker/image_picker.dart';
+import 'package:ugd2_c_kelompok6/screens/profile.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'dart:typed_data';
+import 'package:shared_preferences/shared_preferences.dart';
+
 class ItemProfile extends StatefulWidget {
   const ItemProfile({super.key, required this.id});
-
   final int id;
 
   @override
@@ -20,10 +26,26 @@ class _ItemProfileState extends State<ItemProfile> {
 
   bool isLoading = false;
 
+  int? idUser;
+  Key imageKey = UniqueKey();
+  Uint8List? imageFile;
+  final imagePicker = ImagePicker();
+
   void loadData() async {
     setState(() {
       isLoading = true;
+
+     
     });
+
+    
+
+    Future<void> getIdUser() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    setState(() {
+      idUser = pref.getInt('id')!;
+    });
+  }
 
     try {
       User res = await AuthClient.find(widget.id);
@@ -33,6 +55,11 @@ class _ItemProfileState extends State<ItemProfile> {
         emailController.value = TextEditingValue(text: res.email);
         tanggalLahirController.value = TextEditingValue(text: res.tgl_lahir);
         nomorTeleponController.value = TextEditingValue(text: res.no_telp);
+        
+        ambilData();
+        getIdUser();
+        
+
       });
     } catch (err) {
       // showSnackBar(context, err.toString(), Colors.red);
@@ -44,6 +71,7 @@ class _ItemProfileState extends State<ItemProfile> {
   void initState() {
     super.initState();
 
+    
     if (widget.id != null) {
       loadData();
     }
@@ -55,7 +83,27 @@ class _ItemProfileState extends State<ItemProfile> {
         ? const Center(
             child: CircularProgressIndicator(),
           )
-        : Column(
+        : Scaffold(
+          appBar: AppBar(
+          title: const Text('Profile User'), // Judul AppBar
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => Profile(
+                    id: idUser!,
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+
+
+        body: Column(
             children: [
               const Expanded(flex: 2, child: _TopPortion()),
               Expanded(
@@ -64,6 +112,36 @@ class _ItemProfileState extends State<ItemProfile> {
                   padding: const EdgeInsets.all(8.0),
                   child: Column(
                     children: [
+
+                      GestureDetector(
+                          onTap: () {
+                            showDialog(
+                              context: context,
+                              builder: (context) {
+                                return AlertDialog(
+                                  title: Text('Action on CircleAvatar Tap'),
+                                  content: Text('You tapped the avatar!'),
+                                  actions: <Widget>[
+                                    TextButton(
+                                      onPressed: () {
+                                        getFromGallery();
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: Text('Ambil Dari galery'),
+                                    ),
+                                    TextButton(
+                                      onPressed: () {
+                                        getFromCamera();
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: Text('Ambil Dari kamera'),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          },
+                        ),
                       Text(
                         usernameController.text,
                         style: Theme.of(context)
@@ -132,9 +210,66 @@ class _ItemProfileState extends State<ItemProfile> {
                 ),
               ),
             ],
-          );
+          ),
+        );
   }
+
+   Future<void> ambilData() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+
+    setState(() {
+      usernameController.text = pref.getString('username') ?? '';
+    });
+
+    final isiData = await _getData();
+    if (isiData != null) {
+      usernameController.text = isiData['username'] ?? '';
+      emailController.text = isiData['email'] ?? '';
+      tanggalLahirController.text = isiData['tgl_lahir']??'';
+      nomorTeleponController.text = isiData['no_telp']??'';
+      imageFile = isiData['gambar'] ?? Uint8List(0);
+    }
+   }
+
+   Future<Map<String, dynamic>?> _getData() async {
+   
+  }
+
+   getFromCamera() async {
+    final pickedFile = await imagePicker.pickImage(
+      source: ImageSource.camera,
+      maxWidth: 1800,
+      maxHeight: 1800,
+    );
+    if (pickedFile != null) {
+      Uint8List imageBytes = await pickedFile.readAsBytes();
+
+      setState(() {
+        imageFile = imageBytes;
+        imageKey = UniqueKey();
+      });
+    }
+  }
+
+  getFromGallery() async {
+    final pickedFile = await imagePicker.pickImage(
+      source: ImageSource.gallery,
+      maxWidth: 1800,
+      maxHeight: 1800,
+    );
+    if (pickedFile != null) {
+      Uint8List imageBytes = await pickedFile.readAsBytes();
+
+      setState(() {
+        imageFile = imageBytes;
+        imageKey = UniqueKey();
+      });
+    }
+  }
+
+
 }
+
 
 class _TopPortion extends StatelessWidget {
   const _TopPortion({Key? key}) : super(key: key);
@@ -164,29 +299,29 @@ class _TopPortion extends StatelessWidget {
             child: Stack(
               fit: StackFit.expand,
               children: [
-                // Container(
-                //   decoration: const BoxDecoration(
-                //     color: Colors.black,
-                //     shape: BoxShape.circle,
-                //     image: DecorationImage(
-                //         fit: BoxFit.cover,
-                //         image: NetworkImage(
-                //             'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')),
-                //   ),
-                // ),
-                // Positioned(
-                //   bottom: 0,
-                //   right: 0,
-                //   child: CircleAvatar(
-                //     radius: 20,
-                //     backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-                //     child: Container(
-                //       margin: const EdgeInsets.all(8.0),
-                //       decoration: const BoxDecoration(
-                //           color: Colors.green, shape: BoxShape.circle),
-                //     ),
-                //   ),
-                // ),
+                Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.black,
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                            'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')),
+                  ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  right: 0,
+                  child: CircleAvatar(
+                    radius: 20,
+                    backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+                    child: Container(
+                      margin: const EdgeInsets.all(8.0),
+                      decoration: const BoxDecoration(
+                          color: Colors.green, shape: BoxShape.circle),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
